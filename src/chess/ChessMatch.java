9994e723c961dff2e377d45e6218e6f52da792cf
ViniciusValongo <viniciusvalongo1@hotmail.com>
2023-03-08 17:28:15 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -48,6 +48,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is piece on source position");
 		}
+		if(!board.piece(position).isThereAniPossibleMove()) {
+			throw new ChessException("There is no pissible moves for the chosen piece");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
